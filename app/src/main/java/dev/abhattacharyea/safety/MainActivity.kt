303f70572d3ab77053ccc8f4d03810b12ef31255
Aniket Bhattacharyea <i_abh_esc_wq@protonmail.com>
2019-12-04 21:39:59 +0530

    Change UI
    Signed-off-by: Aniket Bhattacharyea <aniketmail669@gmail.com>

--- app/src/main/java/dev/abhattacharyea/safety/MainActivity.kt
+++ app/src/main/java/dev/abhattacharyea/safety/MainActivity.kt
@@ -15,55 +15,55 @@ import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.startService
 
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "lock_screen"
-            val descriptionText = "Test"
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel("lock_screen", name, importance).apply {
-                description = descriptionText
-            }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			val name = "lock_screen"
+			val descriptionText = "Test"
+			val importance = NotificationManager.IMPORTANCE_HIGH
+			val channel = NotificationChannel("lock_screen", name, importance).apply {
+				description = descriptionText
+			}
+			// Register the channel with the system
+			val notificationManager: NotificationManager =
+				getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+			notificationManager.createNotificationChannel(channel)
+		}
 
 
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
 //            setShowWhenLocked(true)
 //        }
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_map, R.id.navigation_settings
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        val pref = getSharedPreferences("prefs", Context.MODE_PRIVATE)
-
-        if(pref.getBoolean("first_run", true)) {
-            startActivity<MainIntroActivity>()
-            pref.edit().putBoolean("first_run", false).apply()
-            finish()
-        }
+		setContentView(R.layout.activity_main)
+		val navView: BottomNavigationView = findViewById(R.id.nav_view)
+		
+		val navController = findNavController(R.id.nav_host_fragment)
+		// Passing each menu ID as a set of Ids because each
+		// menu should be considered as top level destinations.
+		val appBarConfiguration = AppBarConfiguration(
+			setOf(
+				R.id.navigation_home, R.id.navigation_map, R.id.navigation_settings
+			)
+		)
+		setupActionBarWithNavController(navController, appBarConfiguration)
+		navView.setupWithNavController(navController)
+		
+		val pref = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+		
+		if(pref.getBoolean("first_run", true)) {
+			startActivity<MainIntroActivity>()
+			pref.edit().putBoolean("first_run", false).apply()
+			finish()
+		}
 
 //        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
 //            val i = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
 //            startActivityForResult(i, 100)
 //        }
-
-        startService<LockScreenService>()
-    }
-    
+		
+		startService<LockScreenService>()
+	}
+	
 }
